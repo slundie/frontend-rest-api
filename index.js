a6815a979a7c5afada82a66ce55b3bbd63c87f2c
Sam <hulklundie@gmail.com>
2021-09-04 10:23:56 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const helmet = require("helmet")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const path = require("path")
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@coding-blog.x06au.mongodb.net/blog?retryWrites=true&w=majority`
+
+mongoose.connect(mongoString, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+
+mongoose.connection.on("error", function(error) {
+  if (process.env.NODE_ENV === "development") {
+    console.log(error)
+  }
+})
+
+mongoose.connection.on("open", function() {
+  console.log("Connected to MongoDB database.")
+})
+
+app.use(helmet())
+
+app.use(require("./routes/index.js"))
+
+app.use("/assets", express.static(path.join(__dirname, "..", "..", "assets")))
+
+app.listen(PORT, function () {
+  console.log(`Express app listening on port ${PORT}`)
+})
+
